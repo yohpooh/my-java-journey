8770a9418ab22f10a175489c21ed381dc02188de
yohpooh <new.bramrainier@gmail.com>
2023-10-12 23:50:33 +0800

    updated texture

--- Games/src/main.java
+++ Games/src/main.java
@@ -1,14 +1,19 @@
 import static org.lwjgl.glfw.GLFW.*; //use to import glfw which use for creating window
-
+import org.lwjgl.opengl.GL;
 import org.lwjgl.glfw.GLFWVidMode;
+import static org.lwjgl.opengl.GL11.*;
+
+
 public class main {
-	public static void main(String[] args) {
+	public main()
+	{
 		if(!glfwInit()) {
-			throw new IllegalStateException("Failed to initialize GLFW!");
+			System.err.println("Failed to initialize GLFW!");
+			System.exit(1);
 		}
 		
 		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-		long mainWindow = glfwCreateWindow(640, 480, "LGJGL", 0, 0); //creation of window
+		long mainWindow = glfwCreateWindow(640, 480, "Game 2D Practice", 0, 0); //creation of window
 		if(mainWindow == 0) { // checks if the window is working
 			throw new IllegalStateException("Failed to create window");
 		}
@@ -17,9 +22,59 @@ public class main {
 		glfwSetWindowPos(mainWindow, (videoMode.width() - 640) / 2, (videoMode.height() - 480) / 2); //center window 
 		glfwShowWindow(mainWindow);//show the main window
 		
+		glfwMakeContextCurrent(mainWindow); //to make something on window
+		GL.createCapabilities();
+		glEnable(GL_TEXTURE_2D);
+		texture tex = new texture("./res/texture.png");
+		
+		glClearColor(255, 255, 255, 0); //change the color to white using rgba
+		int colorRed = 0;
+		int colorBlue = 0;
+		int colorGreen = 0;
+		
 		while(!glfwWindowShouldClose(mainWindow)) { //update window until it close
 			glfwPollEvents();
+			if(glfwGetKey(mainWindow, GLFW_KEY_SPACE) == GL_TRUE)
+			{
+				colorRed = 1;
+				colorBlue = 0;
+				colorGreen = 0;
+				glfwSetWindowShouldClose(mainWindow, GL_TRUE);
+			}
+			else 
+			{
+				colorRed = 0;
+				colorBlue = 0;
+				colorGreen = 0;
+			}
+			
+			glClear(GL_COLOR_BUFFER_BIT); //make the window color black and clear everything to black
+			tex.bind();
+			glBegin(GL_QUADS); //insert a square
+				glTexCoord2f(0,0);
+				glVertex2f(-0.5f, 0.5f);
+				glTexCoord2f(1,0);
+				glVertex2f(0.5f, 0.5f);
+				glTexCoord2f(1,1);
+				glVertex2f(0.5f, -0.5f);
+				glTexCoord2f(0,1);
+				glVertex2f(-0.5f, -0.5f);
+
+				//glColor4f(colorRed, colorGreen, colorBlue, 0);
+			glEnd();
+			
+			glfwSwapBuffers(mainWindow); //buffers this is essential to to make everything on windows to render
 		}
 		glfwTerminate(); // clean the whole glfw
 	}
+	
+	private void glfwSetWindowShouldClose(long mainWindow, int glTrue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String[] args) {
+		new main();
+	}
+	
 }
---
+++ Games/src/texture.java
@@ -0,0 +1,61 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import javax.imageio.ImageIO;
+import org.lwjgl.BufferUtils;
+
+import static org.lwjgl.opengl.GL11.*;
+
+
+public class texture {
+	private int id;
+	private int width;
+	private int height;
+	
+	public texture(String filename)
+	{
+		BufferedImage bi;
+		try
+		{
+			bi = ImageIO.read(new File(filename));
+			width = bi.getWidth(); 
+			height = bi.getHeight(); 
+			
+			int[] pixel_raw = new int[width * height * 4];
+			
+			pixel_raw = bi.getRGB(0, 0, width, height, null, 0, width);
+			
+			ByteBuffer pixels = BufferUtils.createByteBuffer(width * height * 4);
+			for (int i = 0; i < width; i++)
+			{
+				for(int j = 0; j < height; j++)
+				{
+					int pixel = pixel_raw[i * width + j];
+					pixels.put((byte)((pixel >> 16) & 0xFF)); //red
+					pixels.put((byte)((pixel >> 8) & 0xFF));  //green
+					pixels.put((byte)((pixel >> 0) & 0xFF));  //blue
+					pixels.put((byte)((pixel >> 24) & 0xFF)); //alpha
+				}
+			}
+			pixels.flip();
+			
+			id = glGenTextures();
+			
+			glBindTexture(GL_TEXTURE_2D, id);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+			
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+			
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void bind()
+	{
+		glBindTexture(GL_TEXTURE_2D, id);
+	}
+}
